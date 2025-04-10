8f74efe10e33cb2215be0c7af1c3cd82c030dcba
Nicotinetroll <105728082+Nicotinetroll@users.noreply.github.com>
2025-04-10 10:25:41 +0200

    Edited Fireball

--- Assets/Common/Scripts/Abilities/Behaviors/Active Abilities/FireBall/FireballProjectileBehavior.cs
+++ Assets/Common/Scripts/Abilities/Behaviors/Active Abilities/FireBall/FireballProjectileBehavior.cs
@@ -1,18 +1,24 @@
 using OctoberStudio.Easing;
+using OctoberStudio.Extensions;
+using OctoberStudio.Pool;
 using UnityEngine;
 using UnityEngine.Events;
+using CartoonFX;
 
 namespace OctoberStudio.Abilities
 {
     public class FireballProjectileBehavior : MonoBehaviour
     {
+        // ✅ Use exact string names from AudioDatabase
         private static readonly int FIREBALL_LAUNCH_HASH = "Fireball Launch".GetHashCode();
         private static readonly int FIREBALL_EXPLOSION_HASH = "Fireball Explosion".GetHashCode();
 
-        [SerializeField] Collider2D fireballCollider;
+        [Header("References")]
+        [SerializeField] private Collider2D fireballCollider;
+        [SerializeField] private GameObject visuals;
 
-        [SerializeField] ParticleSystem explosionParticle;
-        [SerializeField] GameObject visuals;
+        [Header("Settings")]
+        [SerializeField] private string explosionPoolName = "FireballExplosion"; // 👈 Name used in PoolsManager
 
         private IEasingCoroutine movementCoroutine;
         private IEasingCoroutine disableCoroutine;
@@ -32,22 +38,22 @@ namespace OctoberStudio.Abilities
             var distance = Speed * Lifetime * PlayerBehavior.Player.DurationMultiplier;
             var selfDestructPosition = transform.position + transform.rotation * Vector3.up * distance;
 
-            movementCoroutine = transform.DoPosition(selfDestructPosition, Lifetime / PlayerBehavior.Player.ProjectileSpeedMultiplier).SetOnFinish(Explode);
+            movementCoroutine = transform.DoPosition(selfDestructPosition, Lifetime / PlayerBehavior.Player.ProjectileSpeedMultiplier)
+                .SetOnFinish(Explode);
 
             visuals.SetActive(true);
             fireballCollider.enabled = true;
 
+            // ✅ Play correct launch sound
             GameController.AudioManager.PlaySound(FIREBALL_LAUNCH_HASH);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             var enemy = other.GetComponent<EnemyBehavior>();
-
             if (enemy != null)
             {
                 movementCoroutine.Stop();
-
                 Explode();
             }
         }
@@ -58,23 +64,33 @@ namespace OctoberStudio.Abilities
             visuals.SetActive(false);
 
             var enemies = StageController.EnemiesSpawner.GetEnemiesInRadius(transform.position, ExplosionRadius);
-
             for (int i = 0; i < enemies.Count; i++)
             {
-                var enemy = enemies[i];
-
-                enemy.TakeDamage(PlayerBehavior.Player.Damage * DamageMultiplier);
+                enemies[i].TakeDamage(PlayerBehavior.Player.Damage * DamageMultiplier);
             }
 
-            explosionParticle.Play();
+            // ✅ Spawn explosion VFX via pool
+            var fxGO = StageController.PoolsManager.GetEntity(explosionPoolName);
+            if (fxGO != null)
+            {
+                fxGO.transform.position = transform.position;
+                fxGO.transform.rotation = Quaternion.identity;
+                fxGO.SetActive(true);
+
+                if (fxGO.TryGetComponent<CFXR_Effect>(out var fx))
+                    fx.Initialize();
+                else
+                    fxGO.GetComponent<ParticleSystem>()?.Play();
+            }
+
+            // ✅ Play correct explosion sound
+            GameController.AudioManager.PlaySound(FIREBALL_EXPLOSION_HASH);
 
             disableCoroutine = EasingManager.DoAfter(1f, () =>
             {
                 gameObject.SetActive(false);
                 onFinished?.Invoke(this);
             });
-
-            GameController.AudioManager.PlaySound(FIREBALL_EXPLOSION_HASH);
         }
 
         public void Clear()
